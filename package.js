ea4d059570a2a9080c5b2e2f75a1e2941604dd0e
Elliott Spira <elliottspira@gmail.com>
2016-05-05 11:28:07 +1000

    bumping to version 0.1.8

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'em0ney:stub-collection',
-  version: '0.1.7',
+  version: '0.1.8',
   summary: 'A class to stub out your collections when writing unit tests',
   git: 'git@github.com:em0ney/meteor-stub-collection.git',
   documentation: 'README.md'
